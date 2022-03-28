08e95436c37b1873c04bc9fcf0bd51d889b9c751
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2022-03-28 19:10:15 -0400

    Stuff

--- Assets/AppManager.cs
+++ Assets/AppManager.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarChart : MonoBehaviour
+public class AppManager : MonoBehaviour
 {
-    public RectTransform chart;
-    public RectTransform barPrefab;
-
-
     // Start is called before the first frame update
     void Start()
     {
